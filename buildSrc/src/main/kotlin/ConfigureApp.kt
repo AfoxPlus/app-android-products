56e83e83e38a670950fdc4132cd2998986b433b5
Valentin Mendoza <jmendozat13@gmail.com>
2021-11-08 16:14:08 -0500

    Update config

--- buildSrc/src/main/kotlin/ConfigureApp.kt
+++ buildSrc/src/main/kotlin/ConfigureApp.kt
@@ -1,7 +1,7 @@
 object ConfigureApp {
     const val urlRepoDependencies = "https://maven.pkg.github.com/afoxplus/app-yalisto-android"
     const val organization = "afoxplus"
-    const val projectName = "app-android-home"
+    const val projectName = "app-android-products"
     const val applicationId = "app.android.products"
     const val groupId = "com.afoxplus.android"
     const val artifactId = "products"
