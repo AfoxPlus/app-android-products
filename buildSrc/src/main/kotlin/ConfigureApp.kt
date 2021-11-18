dac78beab28b19c69335782b30b0eb4d39e91f54
Valentin Mendoza <jmendozat13@gmail.com>
2021-11-18 01:04:32 -0500

    Update upload artifact

--- buildSrc/src/main/kotlin/ConfigureApp.kt
+++ buildSrc/src/main/kotlin/ConfigureApp.kt
@@ -5,5 +5,5 @@ object ConfigureApp {
     const val applicationId = "app.android.products"
     const val groupId = "com.afoxplus.android"
     const val artifactId = "products"
-    const val version = "1.0.0"
+    const val version = "1.1.0"
 }
