1e8b14e2e09c1ceda4a506a709a96983cf00b391
Valentin Mendoza Torres <jmendozat13@gmail.com>
2022-03-20 00:47:51 -0500

    Feature/refactor dispatcher (#9)
    * Refactor dispatcher
    
    * Update gradle

--- buildSrc/src/main/kotlin/ConfigureApp.kt
+++ buildSrc/src/main/kotlin/ConfigureApp.kt
@@ -5,5 +5,5 @@ object ConfigureApp {
     const val applicationId = "app.android.products"
     const val groupId = "com.afoxplus.android"
     const val artifactId = "products"
-    const val version = "2.0.0"
+    const val version = "2.0.1"
 }
--- module/src/main/java/com/afoxplus/products/delivery/views/fragments/ProductHomeOfferFragment.kt
+++ module/src/main/java/com/afoxplus/products/delivery/views/fragments/ProductHomeOfferFragment.kt
@@ -3,12 +3,18 @@ package com.afoxplus.products.delivery.views.fragments
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import com.afoxplus.products.databinding.FragmentProductsHomeOfferBinding
 import com.afoxplus.products.delivery.viewmodels.ProductViewModel
 import com.afoxplus.products.delivery.views.adapters.ProductAdapter
 import com.afoxplus.products.entities.Product
+import com.afoxplus.uikit.extensions.setGone
 import com.afoxplus.uikit.fragments.BaseFragment
+import com.afoxplus.uikit.views.status.ListEmptyData
+import com.afoxplus.uikit.views.status.ListError
+import com.afoxplus.uikit.views.status.ListLoading
+import com.afoxplus.uikit.views.status.ListSuccess
 
 internal class ProductHomeOfferFragment : BaseFragment() {
     private lateinit var fragmentHomeOfferBinding: FragmentProductsHomeOfferBinding
@@ -27,12 +33,21 @@ internal class ProductHomeOfferFragment : BaseFragment() {
     }
 
     override fun observerViewModel() {
-        productViewModel.productsHomeOffer.observe(viewLifecycleOwner) { products ->
-            productHomeOfferAdapter.submitList(products)
+        productViewModel.productsHomeOffer.observe(viewLifecycleOwner) { state ->
+            when (state) {
+                is ListSuccess -> productHomeOfferAdapter.submitList(state.data)
+                is ListEmptyData -> fragmentHomeOfferBinding.recyclerProductMenu.setGone()
+                is ListLoading -> showToast("Loading...")
+                is ListError -> showToast("Internal Error")
+            }
         }
     }
 
     private fun onClickProductEvent(product: Product) {
         productViewModel.onClickProductOfferEvent(product)
+    }
+
+    private fun showToast(msg: String, duration: Int = Toast.LENGTH_SHORT) {
+        Toast.makeText(requireContext(), msg, duration).show()
     }
 }
